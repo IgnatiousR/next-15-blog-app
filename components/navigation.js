bc7e80832b8f533459e1644c667ca953d8850d17
Ignatious Rozario <ignatiousr80@gmail.com>
2025-06-15 18:02:25 +0600

    Create navigation.js

---
+++ components/navigation.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import styles from "./navigation.module.css"
+
+export default function Navigation(){
+    return (
+        <nav className="font-mono ">
+            <ul className="flex md:space-x-4 flex-col md:flex-row">
+                <li><Link href="/" className="md:p-1 hover:bg-gray-200 hover:text-gray-700 text-2xl md:text-base">Home</Link></li>
+                <li><Link href="/about" className="md:p-1 hover:bg-gray-200 hover:text-gray-700 text-2xl md:text-base">About</Link></li>
+                <li><Link href="/about/projects" className={styles.link}>Projects</Link></li>
+            </ul>
+        </nav>
+    )
+}
