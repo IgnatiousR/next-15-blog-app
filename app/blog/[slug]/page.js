f999e5ad34f0352001ddc2e1efdf358581778186
Ignatious <ignatiousr80@gmail.com>
2025-06-17 12:09:30 +0600

    Update page.js

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,7 +1,27 @@
-export default function BlogPage({params}){
+import { notFound } from "next/navigation"
+import { getPost } from "@/lib/post"
+
+
+export async function generateMetadata({params}) {
+
+    try{
+        const {frontmatter} = await getPost(params.slug)
+        return frontmatter
+    }catch(e){}
+}
+const source = 'Some **mdx** text, with a component'
+
+export default async function BlogPage({params}){
+    let post
+
+    try{
+        post = await getPost(params.slug)
+    } catch(e){notFound}
+
     return (
-        <>
-            Hello from {params.slug}
-        </>
+        <article className="prose dark:prose-invert">
+            {/* <MDXRemote source={post}/> */}
+            {post.content}
+        </article> 
     )
 }
