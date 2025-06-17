77c6042c2c546a54e10ebc0ab4e11144b20b1759
Ignatious <ignatiousr80@gmail.com>
2025-06-17 12:09:46 +0600

    Update header.js

--- components/header.js
+++ components/header.js
@@ -3,14 +3,14 @@ import Navigation from "./navigation";
 
 export default function Header(){
     return (
-        <header className="flex justify-between md:items-center mt-4">
+        <header className="flex justify-between md:items-center mt-4 border-b dark:border-white pb-2">
             <div className="flex items-center md:space-x-12">
                 <div className="hidden md:block">
                     <Link href="/" className="text-xl">User</Link>
                 </div>
                 <Navigation/>
             </div>
-            <div>Dark Toggle</div>
+            <div className="border dark:border-white cursor-pointer p-1">Dark Toggle</div>
         </header>
     )
 }
