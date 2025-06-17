f8a86c2a84d13d995c0998ee178dfda02b863fc0
Ignatious <ignatiousr80@gmail.com>
2025-06-17 12:09:38 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Roboto, Bai_Jamjuree } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 
@@ -12,6 +12,16 @@ const geistMono = Geist_Mono({
     subsets: ["latin"],
 });
 
+const baiJamjuree = Bai_Jamjuree({
+    weight: ['200','300','400','600','700'],
+    subsets: ['latin'],
+})
+
+const roboto = Roboto({
+    weight: ['400', '700'],
+    subsets: ['latin']
+})
+
 export const metadata = {
     title: "Next App",
     description: "next app",
@@ -22,7 +32,9 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+            // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            className={baiJamjuree.className}
+            >
                 <Header/>
                 <main className="mt-12 mx-5">
                     {children}
