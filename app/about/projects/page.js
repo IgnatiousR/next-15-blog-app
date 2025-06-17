379b57d0da327dee7c05689c5ec488d448af4137
Ignatious <ignatiousr80@gmail.com>
2025-06-17 12:09:22 +0600

    Update page.js

--- app/about/projects/page.js
+++ app/about/projects/page.js
@@ -1,6 +1,11 @@
 import { Suspense } from "react";
 import ProjectList from "./components/project-list";
 import ProjectListLoading from "./components/project-list-loading";
+import { ErrorBoundary } from "react-error-boundary";
+
+export const metadata = {
+    title: "Projects",
+};
 
 export default async function ProjectsPage(){
     return (
@@ -8,9 +13,11 @@ export default async function ProjectsPage(){
             <h1 className="mb-8 text-xl">Projects</h1>
             <div className="mb-8">This is the list of projects.</div>
 
-            <Suspense fallback={<ProjectListLoading/>}>
-                <ProjectList/>
-            </Suspense>
+            <ErrorBoundary fallback={<div>Unable to fetch projects currently.</div>}>
+                <Suspense fallback={<ProjectListLoading/>}>
+                    <ProjectList/>
+                </Suspense>
+            </ErrorBoundary>
             
         </div>
     )
