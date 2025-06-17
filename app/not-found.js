7de6e42df75bf9cecca8d0fc4b1e7bb61dc6a475
Ignatious <ignatiousr80@gmail.com>
2025-06-17 12:09:40 +0600

    Create not-found.js

---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+ 
+export default function NotFound() {
+  return (
+    <div>
+      <h1 className='text-2xl font-bold'>404 Not Found</h1>
+      <div>Page not Found.</div>
+    </div>
+  )
+}
